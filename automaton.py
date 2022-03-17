c03fd1ad4d5c2d6d2187f1451f1a321540aec895
vlandero <vlad.andrei.rosu@gmail.com>
2022-03-17 10:09:33 +0200

    first commit

--- automaton.py
+++ automaton.py
@@ -2,33 +2,138 @@ class Automaton():
 
     def __init__(self, config_file):
         self.config_file = config_file
+        self.Sigma = []
+        self.States = []
+        self.Transitions = []
+        self.mat = []
+        self.State_dict = {}
+        self.Word_dict = {}
+        self.Final_states = []
+        self.Start_state = ''
         print("Hi, I'm an automaton!")
 
-    def validate(self):
-        """Return a Boolean
+    def validate(self, input_str):
+        def dfs(node):
+            nonlocal self
+            visited = [False] * len(self.States)
+            v = []
 
-        Returns true if the config file is valid,
-        and raises a ValidationException if the config is invalid.
-        """
-        return "I can't tell if the config file is valid... yet!"
+            def backt(node):
+                nonlocal v
+                nonlocal visited
+                nonlocal self
+                v.append(node)
+                print(node)
+                visited[node] = True
+                for i in range(len(self.States)):
+                    if self.mat[node][i] > 0 and not visited[i]:
+                        backt(i)
+            backt(node)
+
+            return v
+
+        if not self.accepts_input(input_str):
+            return False
+        self.mat = [[0] * len(self.States) for _ in self.States]
+        for t in self.Transitions:
+            self.mat[self.State_dict[t[0]]
+                     ][self.State_dict[t[2]]] = self.Word_dict[t[1]]
+        print(self.mat)
+        print(dfs(self.State_dict[self.Start_state]))
+
+        return True
 
     def accepts_input(self, input_str):
-        """Return a Boolean
+        input_str = input_str.split('\n')
+        lineNum = 0
+        while input_str[lineNum][0] == '#':
+            lineNum += 1
 
-        Returns True if the input is accepted,
-        and it returns False if the input is rejected.
-        """
-        pass
+        if input_str[lineNum] != 'Sigma :':
+            return False
+        lineNum += 1
+        cnt = 1
+        while input_str[lineNum] != 'End':
+            if(lineNum == len(input_str)):
+                return False
+            if input_str[lineNum][0] != '#':
+                if len(input_str[lineNum].split()) > 1:
+                    return False
+                self.Sigma.append(input_str[lineNum].strip())
+                self.Word_dict[input_str[lineNum].strip()] = cnt
+                cnt += 1
+            lineNum += 1
+        lineNum += 1
+        print(f"Sigma: {self.Sigma}")
 
-    def read_input(self, input_str):
-        """Return the automaton's final configuration
-        
-        If the input is rejected, the method raises a
-        RejectionException.
-        """
-        pass
-    
+        while input_str[lineNum][0] == '#':
+            lineNum += 1
+        if input_str[lineNum] != 'States :':
+            return False
+        lineNum += 1
+        cnt = 0
+        while input_str[lineNum] != 'End':
+            if(lineNum == len(input_str)):
+                return False
+            if input_str[lineNum][0] != '#':
+                s = [x.strip() for x in input_str[lineNum].split(',')]
+                if len(s) > 2:
+                    return False
+                if len(s) == 2:
+                    try:
+                        if s[1] not in "FS":
+                            return False
+                    except:
+                        return False
+                    if s[1] == 'F':
+                        self.Final_states.append(s[0])
+                    if s[1] == 'S':
+                        if self.Start_state != '':
+                            return False
+                        self.Start_state = s[0]
+                self.States.append(s[0])
+                self.State_dict[s[0]] = cnt
+                cnt += 1
+            lineNum += 1
+        lineNum += 1
+        print(f"States: {self.States}")
+
+        while input_str[lineNum][0] == '#':
+            lineNum += 1
+
+        if input_str[lineNum] != 'Transitions :':
+            return False
+        lineNum += 1
+        while input_str[lineNum] != 'End':
+            if(lineNum == len(input_str)):
+                return False
+            if input_str[lineNum][0] != '#':
+                try:
+                    stateX, wordY, stateZ = [
+                        x.strip() for x in input_str[lineNum].split(',')]
+                except:
+                    return False
+                if stateX not in self.States or wordY not in self.Sigma or stateZ not in self.States:
+                    return False
+                self.Transitions.append((stateX, wordY, stateZ))
+            lineNum += 1
+        print(f"Transitions: {self.Transitions}")
+        return True
+
+    def read_input(self):
+        fin = open(self.config_file)
+        return fin.read()
+
+    def print_details(self):
+        print("\nStates:")
+        for key, value in self.State_dict.items():
+            print(f"{key} -> {value}")
+        print("\nWords:")
+        for key, value in self.Word_dict.items():
+            print(f"{key} -> {value}")
+
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
-    print(a.validate())
+    a = Automaton('file.txt')
+    print(a.validate(a.read_input()))
+    a.print_details()
